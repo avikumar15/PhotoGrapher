35ccc79c6587e88a4f7c92a080f97b81c6b5bade
avikumar15 <avithewinner1508@gmail.com>
2019-11-06 15:40:42 +0530

    [ADD] Camera Activity and Main Activity - Initial code to launch camera activity and main activity

--- app/src/main/java/com/example/photographer/MainActivity.java
+++ app/src/main/java/com/example/photographer/MainActivity.java
@@ -2,13 +2,42 @@ package com.example.photographer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText inputText;
+    Button graphIt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        inputText = (EditText) findViewById(R.id.inputText);
+        graphIt = (Button) findViewById(R.id.graphIt);
+
+        inputText.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                final int DRAWABLE_RIGHT = 2;
+
+                if(event.getAction() == MotionEvent.ACTION_UP) {
+                    if(event.getRawX() >= (inputText.getRight() - inputText.getCompoundDrawables()[DRAWABLE_RIGHT].getBounds().width())) {
+
+                        Intent intent = new Intent(MainActivity.this,CameraActivity.class);
+                        startActivity(intent);
+
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
     }
 }
