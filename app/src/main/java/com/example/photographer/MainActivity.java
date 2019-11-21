6d1f7430e67874a2abe841f381c7bad03484cc01
CaptainIRS <36656347+CaptainIRS@users.noreply.github.com>
2019-11-21 23:23:13 +0530

    Added equation input functionality. Modified MainActivity layout.

--- app/src/main/java/com/example/photographer/MainActivity.java
+++ app/src/main/java/com/example/photographer/MainActivity.java
@@ -1,43 +1,63 @@
 package com.example.photographer;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MotionEvent;
 import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText inputText;
+    ImageButton cameraButton;
     Button graphIt;
+    WebView webView;
+    String latexString;
 
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        inputText = (EditText) findViewById(R.id.inputText);
-        graphIt = (Button) findViewById(R.id.graphIt);
+        cameraButton = findViewById(R.id.cameraButton);
+        graphIt = findViewById(R.id.graphIt);
+        webView = findViewById(R.id.mathWebView);
+        latexString = "\\\\frac{-b\\\\pm\\\\sqrt{b^2-4ac}}{2a}";
 
-        inputText.setOnTouchListener(new View.OnTouchListener() {
+        /*
+         * The app uses MathQuill, a javascript based latex editor
+         * One may ask, why use a JS library when libraries like jLatexmath exist for java.
+         * But, none of such libraries for java are WYSIWYG. They require LaTex to be hardcoded
+         * or expect the user to enter LaTex. This app is intended to make data entry easier and
+         * must be WYSIWYG. Though WebView is used, the app is completely offline.
+         */
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.requestFocus(View.FOCUS_DOWN);
+        webView.loadUrl("file:///android_asset/math_text_box.html");
+        cameraButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                final int DRAWABLE_RIGHT = 2;
-
-                if(event.getAction() == MotionEvent.ACTION_UP) {
-                    if(event.getRawX() >= (inputText.getRight() - inputText.getCompoundDrawables()[DRAWABLE_RIGHT].getBounds().width())) {
-
-                        Intent intent = new Intent(MainActivity.this,CameraActivity.class);
-                        startActivity(intent);
-
-                        return true;
-                    }
-                }
-                return false;
+            public void onClick(View v) {
+                onCameraButtonClick();
             }
         });
+    }
+
+    private void onCameraButtonClick() {
+        /*
+         * For the time being let the string 'latexString' be returned from the API that retrieves
+         * the math equations from the image. Now, we have to pass this into the webView and update
+         * the contents of the math input box. This is demonstrated by passing a test LaTex string
+         * to the webView.
+         */
+        webView.loadUrl("javascript:updateLatex(\"" + latexString + "\")");
+        Intent intent = new Intent(MainActivity.this, CameraActivity.class);
+        startActivity(intent);
     }
 }
